f053897b25f142ae95cb9da6016b0f5fdc3678d7
Hita Anil Kumar <>
2022-04-03 23:26:17 +0530

    Navbar indentation fixed and comments added

--- components/Nav.js
+++ components/Nav.js
@@ -11,21 +11,18 @@ function NavLink({to, children}) {
     </p>
 }
 
+{/*Navigation bar for small screens */}
 function MobileNav({open, setOpen}) {
     return (
         <div className={`absolute top-0 left-0 h-max w-screen bg-headerColor transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
             <div className="flex items-center justify-center filter drop-shadow-md bg-gradient-to-r from-navLine1 to-navLine2 w-full h-96"> {/*logo container*/}
             </div>
+            
+             {/*Links*/}
             <div className="flex flex-col ml-4 h-1/10 items-center ">  
-            <p className=" mt-[10px] font-Manrope not-italic font-normal text-lg text-white leading-5" href="/Lorem" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    Lorem
-                </p>
-                <p className="mt-[20px] font-Manrope not-italic font-normal text-lg text-white leading-5" href="/Lorem" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    Lorem
-                </p>
-                <p className="mt-[20px] mb-[10px] font-Manrope not-italic font-normal text-lg text-white leading-5" href="/Lorem" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    Lorem
-                </p>
+                <p className=" mt-[10px] font-Manrope not-italic font-normal text-lg text-white leading-5" href="/Lorem" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>Lorem</p>
+                <p className="mt-[20px] font-Manrope not-italic font-normal text-lg text-white leading-5" href="/Lorem" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>Lorem</p>
+                <p className="mt-[20px] mb-[10px] font-Manrope not-italic font-normal text-lg text-white leading-5" href="/Lorem" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>Lorem</p>
             </div>  
         </div>
     )
@@ -35,11 +32,12 @@ export default function Nav() {
 
     const [open, setOpen] = useState(false)
     return (
-        <div className='msm: h-[200px] md:h-96 bg-gradient-to-r from-rangoon via-tuna to-rhino'>
+    <div className='msm: h-[200px] md:h-96 bg-gradient-to-r from-rangoon via-tuna to-rhino'>
         <nav className="flex filter drop-shadow-md bg-headerColor h-96 px-4 py-4 h-20 items-center">
             <MobileNav open={open} setOpen={setOpen}/>
-            <div className="w-1/2 flex items-center">
 
+            {/* main logo */}
+            <div className="w-1/2 flex items-center">
             <div className="ml-40">
             <Image
             src={whiteLogo}
@@ -51,42 +49,38 @@ export default function Nav() {
             </div>
 
             <div className="w-9/12 flex justify-end items-center">
-
-            <div className="relative rounded-md shadow-sm  max-w-657 w-85p ml-1/20">
-            <div className="absolute inset-y-0 left-0 pl-19 pt-8 flex items-center pointer-events-none">
-              <button>
-              <Image
-              src={search}
-              alt="Search"
-              width={17.77}
-              height={17.77}
-              />
-              </button>             
-            </div>
-            <input type="text" className="w-full focus:outline-none focus:border-dark-jungle focus:ring-dark-jungle block indent-37 h-36 pl-19 pr-12 sm:text-sm bg-dark-jungle shadow-navSearch rounded-10 font-Manrope not-italic font-normal text-13 text-dawn leading-18" placeholder="Lorem ipsum">
-            </input>              
-          </div>
-
-                <div className="z-50 flex relative ml-40 w-20 h-20 flex-col justify-between items-center md:hidden" onClick={() => {
-                    setOpen(!open)
-                }}>
-             
+                <div className="relative rounded-md shadow-sm  max-w-657 w-85p ml-1/20">
+                    <div className="absolute inset-y-0 left-0 pl-19 pt-8 flex items-center pointer-events-none">
+                        <button>
+                            <Image
+                            src={search}
+                            alt="Search"
+                            width={17.77}
+                            height={17.77}
+                            />
+                        </button>             
+                    </div>
+                    <input type="text" className="w-full focus:outline-none focus:border-dark-jungle focus:ring-dark-jungle block indent-37 h-36 pl-19 pr-12 sm:text-sm bg-dark-jungle shadow-navSearch rounded-10 font-Manrope not-italic font-normal text-13 text-dawn leading-18" placeholder="Lorem ipsum">
+                    </input>              
+                 </div>
+                
+                {/*hambyrger menu */}
+                <div className="z-50 flex relative ml-40 w-20 h-20 flex-col justify-between items-center md:hidden" onClick={() => {setOpen(!open) }}>
                     <span className={`h-1 mr-[20px] w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
                     <span className={`h-1 mr-[20px] w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
                     <span className={`h-1 mr-[20px] w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
                 </div>
                 
-
+                {/* Links */}
                 <div className=" hidden md:flex">
-                <div className="mx-10 ml-42 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>
-                <div className="mx-10 ml-42 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>
-                <div className="mx-10 ml-42 mr-42 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>
-                     
+                    <div className="mx-10 ml-42 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>
+                    <div className="mx-10 ml-42 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>
+                    <div className="mx-10 ml-42 mr-42 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>                     
                 </div>
             </div>
         </nav>
-        <div className="z-40 w-full h-6 bg-gradient-to-r from-navLine1 to-navLine2 w-full md:visible">
-        </div>
-        </div>
+            <div className="z-40 w-full h-6 bg-gradient-to-r from-navLine1 to-navLine2 w-full md:visible">
+            </div>
+    </div>
     )
 }
