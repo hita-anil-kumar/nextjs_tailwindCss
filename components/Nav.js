f03efb29ac61c4edb364ccca42d1df7bd28153bd
Hita Anil Kumar <>
2022-04-04 03:24:42 +0530

    code clean up for navigation bar

--- components/Nav.js
+++ components/Nav.js
@@ -17,7 +17,7 @@ function MobileNav({open, setOpen}) {
         <div className={`absolute top-0 left-0 h-max w-screen bg-headerColor transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
             <div className="flex items-center justify-center filter drop-shadow-md bg-gradient-to-r from-navLine1 to-navLine2 w-full h-96"> {/*logo container*/}
             </div>
-            
+
              {/*Links*/}
             <div className="flex flex-col ml-4 h-1/10 items-center ">  
                 <p className=" mt-[10px] font-Manrope not-italic font-normal text-lg text-white leading-5" href="/Lorem" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>Lorem</p>
@@ -33,11 +33,11 @@ export default function Nav() {
     const [open, setOpen] = useState(false)
     return (
     <div className='msm: h-[200px] md:h-96 bg-gradient-to-r from-rangoon via-tuna to-rhino'>
-        <nav className="flex filter drop-shadow-md bg-headerColor h-96 px-4 py-4 h-20 items-center">
+        <nav className="flex bg-headerColor h-96 items-center">
             <MobileNav open={open} setOpen={setOpen}/>
 
             {/* main logo */}
-            <div className="w-1/2 flex items-center">
+            <div className="w-1/2">
             <div className="ml-40">
             <Image
             src={whiteLogo}
@@ -47,9 +47,10 @@ export default function Nav() {
             />
             </div>
             </div>
-
+          
+            {/*Search bar */}
             <div className="w-9/12 flex justify-end items-center">
-                <div className="relative rounded-md shadow-sm  max-w-657 w-85p ml-1/20">
+                <div className="relative max-w-657 w-85p ml-1/20">
                     <div className="absolute inset-y-0 left-0 pl-19 pt-8 flex items-center pointer-events-none">
                         <button>
                             <Image
@@ -60,11 +61,11 @@ export default function Nav() {
                             />
                         </button>             
                     </div>
-                    <input type="text" className="w-full focus:outline-none focus:border-dark-jungle focus:ring-dark-jungle block indent-37 h-36 pl-19 pr-12 sm:text-sm bg-dark-jungle shadow-navSearch rounded-10 font-Manrope not-italic font-normal text-13 text-dawn leading-18" placeholder="Lorem ipsum">
+                    <input type="text" className="w-full focus:outline-none focus:border-dark-jungle focus:ring-dark-jungle block indent-37 h-45 pl-19 pr-12 sm:text-sm bg-dark-jungle shadow-navSearch rounded-10 font-Manrope not-italic font-semibold text-13 text-dawn leading-17.76" placeholder="Lorem ipsum">
                     </input>              
                  </div>
                 
-                {/*hambyrger menu */}
+                {/*hamburger menu */}
                 <div className="z-50 flex relative ml-40 w-20 h-20 flex-col justify-between items-center md:hidden" onClick={() => {setOpen(!open) }}>
                     <span className={`h-1 mr-[20px] w-full bg-white rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
                     <span className={`h-1 mr-[20px] w-full bg-white rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
@@ -73,9 +74,9 @@ export default function Nav() {
                 
                 {/* Links */}
                 <div className=" hidden md:flex">
-                    <div className="mx-10 ml-42 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>
-                    <div className="mx-10 ml-42 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>
-                    <div className="mx-10 ml-42 mr-42 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>                     
+                    <div className="mx-10 ml-41 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>
+                    <div className="mx-10 ml-41 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>
+                    <div className="mx-10 ml-41 mr-42 w-47 h-20 font-Manrope not-italic font-extrabold text-15 text-white leading-5 hover:text-redpink"><Link href="/Lorem">Lorem</Link></div>                     
                 </div>
             </div>
         </nav>
